a753868ba557d97151f65cf84e06b62828a84c3e
Shohjahon707 <shohjahonabdullayev17@gmail.com>
2024-05-01 22:04:42 +0500

    ketdi

---
+++ doctors.html/js/leson.js
@@ -0,0 +1,6 @@
+var a =10
+let ism = prompt("Ism kiriting")
+let familiya = prompt("Familya kiriting")
+let yosh = +prompt("Yosh kiriting")
+let manzil = prompt("Manzil kiriting")
+  alert(ism,familiya,yosh,manzil)
---
+++ doctors.html/js/lesson2.js
@@ -0,0 +1,23 @@
+// let name = 'Shohjahon'
+// let fullName ="Sayfullayev"
+// let age = 465
+// let isActive = true
+
+
+//    let L=+prompt("Santimetr kiriting")
+//    alert(Math.floor(L/100))
+
+//    let M=+prompt("Kgram kiriting")
+//    alert(Math.floor(M/1000)) 
+
+//    let H=+prompt("Bayt kiriting")
+//    alert(Math.floor(H/1024))
+
+   //  let a=+prompt("Raqam kiriting")
+   //  let b=a%10
+   //  let c=Math.floor(a/10)
+   //  alert(`${b+c}`)
+   //  alert ("ikki xonali sonning raqamlar yigindisi bo'ladi"+ $ (b+c)  )
+   
+     
+
